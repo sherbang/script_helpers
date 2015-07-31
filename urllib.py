858cca3d71361862683354a75f9a3062d2c21d95
Brian Johnson <brian@sherbang.com>
2015-07-31 16:07:00 -0400

    Functions to safely download a file from a remote server.
    The file gets a sanitized version of the name the server specifies.

---
+++ urllib.py
@@ -0,0 +1,63 @@
+from werkzeug.utils import secure_filename
+
+import cgi
+import logging
+import os
+import posixpath
+import urllib.request
+import shutil
+import urllib.parse
+
+logger = logging.getLogger('sherbang.script_helpers')
+
+
+class FileExistsExeption(Exception):
+    def __init__(self, filename):
+        super().__init__('{} already exists'.format(filename))
+        self.filename = filename
+
+
+def get_filename(urllib_response):
+    """
+    Attempt to get the filename out of the response headers.  Failing that, use
+    the filename part of the URL.  Sanitize the filename to make it safe to use
+    locally.
+    """
+    cd_header = urllib_response.headers.get('Content-Disposition', '')
+    _, params = cgi.parse_header(cd_header)
+    if 'filename' in params:
+        filename = params['filename']
+    else:
+        path = urllib.parse.urlsplit(urllib_response.geturl()).path
+        filename = posixpath.basename(path)
+
+    filename = secure_filename(filename)
+
+    return filename
+
+
+def download_file(url, output_dir=None, overwrite=False):
+    """
+    Download the file from `url` and save it locally under `output_dir`.
+
+    If overwrite is True then existing files matching the same filename will
+    be overwritten, otherwise a FileExistsExeption will be raised.
+    """
+    with urllib.request.urlopen(url) as response:
+        file_name = get_filename(response)
+        if output_dir:
+            file_name = os.path.join(output_dir, file_name)
+
+        if overwrite is not True and os.path.exists(file_name):
+            exc = FileExistsExeption(
+                'File already exists: "{}"'.format(file_name))
+            exc.filename = file_name
+            raise exc
+        else:
+            logger.info(
+                'Downloading {} to {}'.format(response.geturl(), file_name)
+                )
+            with open(file_name, 'wb') as out_file:
+                shutil.copyfileobj(response, out_file)
+
+    return file_name
